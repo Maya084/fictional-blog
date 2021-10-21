88b920a1db88eed2a0d70c831e969a5c6e43bf56
Maja <maja.nikolovska@ved.mk>
2021-10-21 10:03:32 +0200

    Auto-generated commit [ci skip]

---
+++ assets/constants.ts
@@ -0,0 +1,10 @@
+const rootUrl = "https://jsonplaceholder.typicode.com/";
+
+export const URLS = {
+    POSTS: rootUrl + 'posts',
+    USERS: rootUrl + 'users',
+    COMMENTS: rootUrl + 'comments',
+    PHOTOS: rootUrl + 'photos'
+};
+
+export const LOCAL_STORAGE_KEY = "FavoritePosts" as string;
